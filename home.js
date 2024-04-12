204de31e7e22ab5dd487a31886cb47799a5a8356
Isa <124785311+isacaramigo@users.noreply.github.com>
2024-04-12 17:19:38 -0300

    home.js

--- home.js
+++ home.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { View, Text, Image, Button } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { styles } from '../style/home.js';
+import { View, Image, Text, Pressable} from 'react-native';
+import { styles } from './style/home.js';
 
-export default function Home() {
-    return(
-        <View style={styles.container}>
-            <Image
-            style={styles.logo}
-            source={require('./assets/image/logoVanmooshFundoTransparente.png')}
-            />
-                <View style={styles.linha1}></View>
-            <StatusBar style="auto"/>
-            <Button style={styles.agregado}>Ana</Button>
-        </View>
-    );
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <Image
+        style={styles.logo}
+        source={require('./assets/image/logoVanmooshFundoTransparente.png')}
+        />
+        <View style={styles.linha1}></View>
+          <StatusBar style="auto" />
+            <Pressable><Text style={styles.botao}> > Ana</Text></Pressable>
+            <View style={styles.linha2}></View>
+            <Pressable><Text style={styles.botao}> > João</Text></Pressable>
+    </View>
+    
+  );
 }
