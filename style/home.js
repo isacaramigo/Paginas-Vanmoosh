189bea1e659111222fb8befb276277aff17487f3
Isa <124785311+isacaramigo@users.noreply.github.com>
2024-04-12 16:53:36 -0300

    Add files via upload

---
+++ style/home.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFF5',
+        alignItems: 'center',
+      },
+  
+      logo: {
+        marginTop: 40,
+        height: 110,
+        width: 210,
+      },
+  
+      linha1: {
+        marginTop: -20,
+        height: 1,
+        width: 500,
+        backgroundColor: '#D9D9D9',
+      },
+});
