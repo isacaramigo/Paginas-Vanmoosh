65d2f382d74412d18c1f7606bd2158346ce19961
Isa <124785311+isacaramigo@users.noreply.github.com>
2024-04-12 23:15:12 -0300

    Add files via upload

--- home.js
+++ home.js
@@ -1,20 +1,18 @@
+import React from 'react';
+import { View, Text, Image, Button } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { View, Image, Text, Pressable} from 'react-native';
-import { styles } from './style/home.js';
+import { styles } from '../style/home.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-        <Image
-        style={styles.logo}
-        source={require('./assets/image/logoVanmooshFundoTransparente.png')}
-        />
-        <View style={styles.linha1}></View>
-          <StatusBar style="auto" />
-            <Pressable><Text style={styles.botao}> > Ana</Text></Pressable>
-            <View style={styles.linha2}></View>
-            <Pressable><Text style={styles.botao}> > João</Text></Pressable>
-    </View>
-    
-  );
+export default function Home() {
+    return(
+        <View style={styles.container}>
+            <Image
+            style={styles.logo}
+            source={require('./assets/image/logoVanmooshFundoTransparente.png')}
+            />
+                <View style={styles.linha1}></View>
+            <StatusBar style="auto"/>
+            <Button style={styles.agregado}>Ana</Button>
+        </View>
+    );
 }
